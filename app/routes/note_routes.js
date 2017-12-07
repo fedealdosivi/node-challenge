39b179a32e831e2ebcd1ea4673fd3cf65df96026
Federico Palomero Garcia <fedepalo13@gmail.com>
2017-12-07 15:51:26 -0300

    req by headers working

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,2 +1,6 @@
 module.exports = function(app, db) {
+	app.post('/notes', (req, res) => {
+		console.log(req.headers)
+    	res.send('Hello')
+  });
 };
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const app            = express();
+const port = 8000;
 
-const port = 8002;
+require('./app/routes')(app, {});
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(port, () => {
   console.log('We are live on ' + port);
 });
